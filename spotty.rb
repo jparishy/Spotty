f3ba08f229495a19dde6cf4081bc1513b95fbd23
Julius Parishy <juliusparishy@gmail.com>
2012-01-30 23:33:50 -0500

    Added documentation to the file. Still needs work, but we're getting there.

--- spotty.rb
+++ spotty.rb
@@ -3,46 +3,140 @@
 
 module Spotify
   
+  #
+  # The following two methods interface with Spotify's AppleScript integration.
+  # You can find the documentation on the exported AppleScript functions
+  # by loading AppleScript Editor, opening the Library window, then adding the
+  # Spotify.app file to the list. This will show you the library functions 
+  # for Spotify. All the information used to create this program was found
+  # through this documentation.
+  #
+  
+  #
+  # Spotify.do_something(something)
+  #
+  # Tell Spotify to perform an operation. Additionally strips off the newline
+  # character at the end of the result.
+  #
+  # For Example:
+  # Spotify.do_something("play")
+  #
+  # ...generates...
+  #
+  # tell application "Spotify" to #{something}
+  #
+  # ...will result in Spotify playing the current song.
+  #
   def Spotify.do_something(something)
     `osascript -e 'tell application \"Spotify\" to #{something}'`.gsub("\n", "")
   end
   
+  #
+  # Spotify.get_something(something)
+  #
+  # Sets up a string that consists of AppleScript that tells Spotify to
+  # return the value for the query 'something'. Additionally strips off the newline
+  # character at the end of the result.
+  #
+  # For Example:
+  # Spotify.get_something("player state")
+  #
+  # ...generates...
+  #
+  # tell application "Spotify"
+  #   player state
+  # end tell
+  #
+  # ...which might return...
+  #
+  # "paused"
+  #
   def Spotify.get_something(something)
     `osascript -e 'tell application \"Spotify\"\n\t#{something}\nend tell\n'`.gsub("\n", "")
   end
   
+  #
+  # Spotify.playing?
+  #
+  # Is the Spotify music player currently playing a song?
+  #
   def Spotify.playing?()
     Spotify.get_something("player state") == "playing"
   end
   
+  #
+  # Spotify.paused?
+  #
+  # Is the Spotify music player paused?
+  #
   def Spotify.paused?()
     Spotify.get_something("player state") == "paused"
   end
   
+  #
+  # Spotify.stopped?
+  #
+  # Is the Spotify music player stopped?
+  #
   def Spotify.stopped?()
     Spotify.get_something("player state") == "stopped"
   end
   
+  #
+  # Spotify.play
+  #
+  # Play a currently paused song. No action taken if
+  # no song is queued up, ie. Spotify was just started.
+  #
   def Spotify.play()
     Spotify.do_something("play")
   end
   
+  #
+  # Spotify.pause
+  #
+  # Pause the currently playing song.
+  #
   def Spotify.pause()
     Spotify.do_something("pause")
   end
 
+  #
+  # Spotify.toggle_playing
+  #
+  # If the song is paused, play it.
+  # If the song is playing, pause it.
+  #
   def Spotify.toggle_playing()
     Spotify.do_something("playpause")
   end
   
+  #
+  # Spotify.next
+  #
+  # Skip the currently playing track and play the next one.
+  #
   def Spotify.next()
     Spotify.do_something("play next track")
   end
   
+  #
+  # Spotify.previous
+  #
+  # Play the previous song, or restart the current one if the
+  # playback position is after a certain threshold. This is
+  # normal media player behavior and part of Spotify.
+  #
   def Spotify.previous()
     Spotify.do_something("play previous track")
   end
   
+  #
+  # Spotify.song_position
+  #
+  # Prints out the playback time of the current song and the duration
+  # in the format "<current time>/<duration>"
+  #
   def Spotify.song_position()
     current = Spotify.get_something("player position").to_i
     total = Spotify.get_something("duration of current track").to_i
@@ -50,6 +144,12 @@ module Spotify
     puts "#{current}/#{total}"
   end
   
+  #
+  # Spotify.now_playing
+  #
+  # Returns an array containing the name of the currently playing song
+  # and the artist of the song
+  #
   def Spotify.now_playing()
     name   = Spotify.get_something("name of current track")
     artist = Spotify.get_something("artist of current track")
@@ -59,6 +159,12 @@ module Spotify
   
 end
 
+#
+# print_help
+#
+# Print out some information for when the program is run with the 'help'
+# option or the option isn't recognized.
+#
 def print_help()
   puts "\nusage: spotty [args]"
   puts "A command line interface to Spotify on Mac OS X."
@@ -79,6 +185,11 @@ def print_help()
   puts "\n\nThank you for using spotty!"
 end
 
+#
+# Let's go ahead and loop through all of the passed arguments and
+# perform each operation sequentially. This can be fancied up to support
+# arguments in the future.
+#
 ARGV.each do |arg|
   case arg
   when "play"
